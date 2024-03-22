2bf78e91ef74b7b8b376f660558b028ae29bfd6d
SalmanShahid1985 <SalmanShahid1985@gmail.com>
2024-03-22 11:17:22 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          axios.post("https://ovasaveapistage.azurewebsites.net/v1/contact-us", {
+            name: "ali",
+            email: "abc@gmail.com",
+            description: "jfeovouevoeuvoevoevun",
+          });
+        }}
+      >ali bhai</button>
     </div>
   );
 }
